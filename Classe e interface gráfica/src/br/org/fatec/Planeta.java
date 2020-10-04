eb3ad13c871547cb16b88d0b633c302ecb8f4cf5
Junior Forneiro <jose.forneiro@fatec.sp.gov.br>
2020-10-04 19:35:14 -0300

    Adicionada a classe Planeta

---
+++ Classe e interface gráfica/src/br/org/fatec/Planeta.java
@@ -0,0 +1,66 @@
+package br.org.fatec;
+
+public class Planeta {
+	String nome;
+	String cor;
+	String tamanho;
+	Float distancia_sol;
+	String habitado;
+	
+	//Construtor
+	public Planeta(String nomeplaneta, String habitado) {
+		nome = nomeplaneta;
+		this.habitado = habitado;
+	}
+	
+	//Metodos de acesso dos tipos Getter e Setter
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public Float getDistancia_sol() {
+		return distancia_sol;
+	}
+
+	public void setDistancia_sol(Float distancia_sol) {
+		this.distancia_sol = distancia_sol;
+	}
+
+	public String getHabitado() {
+		return habitado;
+	}
+
+	public void setHabitado(String habitado) {
+		this.habitado = habitado;
+	}
+	
+	// Frio
+	public void frio() {
+		System.out.println("Esse planeta é muito frio para ser habitado");
+	}
+	
+	// Quente
+		public void quente() {
+			System.out.println("Esse planeta é muito frio para ser habitado");
+		}
+}
